702d59a30e90215a35c53b9d9c87b2d6668d2c42
cisuarez <117554825+cisuarez@users.noreply.github.com>
2023-08-17 15:27:29 -0500

    Create MostrarRegistrosOpcion.java

---
+++ src/ProcesoPrincipal/MostrarRegistrosOpcion.java
@@ -0,0 +1,5 @@
+class MostrarRegistrosOpcion implements OpcionMenu {
+    public void ejecutar(RegistroSocios registroSocios, Scanner eleccion) {
+        registroSocios.mostrarRegistros();
+    }
+}
